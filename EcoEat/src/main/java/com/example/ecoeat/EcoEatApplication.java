29edbd410c991b5dbc6bcb86b88dae96f20b3e9f
Mingxuan Chen <mche0126@student.monash.edu>
2021-08-21 13:10:26 +1000

    Create Project

---
+++ EcoEat/src/main/java/com/example/ecoeat/EcoEatApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoEatApplication.class, args);
+    }
+
+}
---
+++ EcoEat/src/test/java/com/example/ecoeat/EcoEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecoeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
