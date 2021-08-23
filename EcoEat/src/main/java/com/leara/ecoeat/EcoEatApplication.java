81c633faf666504d9686b068af8281c60f52c9d8
Mingxuan Chen <mche0126@student.monash.edu>
2021-08-23 23:14:48 +1000

    Hello World in Spring boot

---
+++ EcoEat/src/main/java/com/leara/ecoeat/EcoEatApplication.java
@@ -0,0 +1,21 @@
+package com.leara.ecoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EcoEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoEatApplication.class, args);
+    }
+
+    @GetMapping
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World")String name) {
+        return String.format("Hello %s!", name);
+    }
+}
