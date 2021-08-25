12a49e48863fe3ada4245695f7ec86bd7d0ac461
LAKSHMI U <lunn0001@student.monash.edu>
2021-08-25 16:24:10 +0530

    --View redirect for Tips page --Header added to Home and Tips

--- src/main/java/com/leara/ecoeat/EcoEatApplication.java
+++ src/main/java/com/leara/ecoeat/EcoEatApplication.java
@@ -2,20 +2,27 @@ package com.leara.ecoeat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class EcoEatApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcoEatApplication.class, args);
     }
 
-    @GetMapping
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World")String name) {
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+    @RequestMapping("/tips")
+    public String viewTips() {
+        return "tips";
     }
 }
